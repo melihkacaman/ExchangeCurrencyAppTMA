15340c16ddc4e66828c1898bab5bfe318a3f1b75
melih <melih.kacaman@stu.fsm.edu.tr>
2021-10-29 21:42:52 +0300

    structure of main thread

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,36 @@
+import com.googlecode.lanterna.gui2.MultiWindowTextGUI;
+import com.googlecode.lanterna.gui2.WindowBasedTextGUI;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
+        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory();
+        Screen screen = null;
 
+        try {
+            screen = terminalFactory.createScreen();
+            screen.startScreen();
+
+            final WindowBasedTextGUI textGUI = new MultiWindowTextGUI(screen);
+
+            //textGUI.addWindowAndWait(new window(textGUI));
+            //textGUI.addWindowAndWait(new window("lorem ipsum"));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            if(screen != null) {
+                try {
+                    screen.stopScreen();
+                }
+                catch(IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
